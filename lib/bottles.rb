2c1b7bef8d9b23ca1fe0b222edab7e25b6bc5c53
Emmanuel Milou <emmanuel.milou@shopify.com>
2019-05-12 18:33:24 -0400

    Implements Shameless Green version

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,38 +1,34 @@
 class Bottles
-  def initialize
+  def song
+    verses(99, 0)
+  end
+
+  def verses(starting, ending)
+    starting.downto(ending).collect {|i| verse(i)}.join("\n")
   end
 
   def verse(number)
-    if number == 0
-      first_line = "No more bottles of beer on the wall, no more bottles of beer.\n"
-    elsif number == 1
-      first_line = "#{number} bottle of beer on the wall, #{number} bottle of beer.\n"
+    case number
+    when 0
+      "No more bottles of beer on the wall, " +
+      "no more bottles of beer.\n" +
+      "Go to the store and buy some more, " +
+      "99 bottles of beer on the wall.\n"
+    when 1
+      "1 bottle of beer on the wall, " +
+      "1 bottle of beer.\n" +
+      "Take it down and pass it around, " +
+      "no more bottles of beer on the wall.\n"
+    when 2
+      "2 bottles of beer on the wall, " +
+      "2 bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "1 bottle of beer on the wall.\n"
     else
-      first_line = "#{number} bottles of beer on the wall, #{number} bottles of beer.\n"
+      "#{number} bottles of beer on the wall, " +
+      "#{number} bottles of beer.\n" +
+      "Take one down and pass it around, " +
+      "#{number - 1} bottles of beer on the wall.\n"
     end
-
-    if number == 2
-      second_line = "Take one down and pass it around, #{number - 1} bottle of beer on the wall.\n"
-    elsif number == 1
-      second_line = "Take it down and pass it around, no more bottles of beer on the wall.\n"
-    elsif number == 0
-      second_line = "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
-    else
-      second_line = "Take one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
-    end
-
-    first_line + second_line
-  end
-
-  def verses(start, final)
-    final_verses = [] 
-    (final..start).each do |number|
-      final_verses.push(verse(number))
-    end
-    final_verses.reverse.join("\n")
-  end
-
-  def song
-    verses(99, 0)
   end
 end
